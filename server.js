836c34d69ea5a17fe5bc1369e115e7c36067c7d6
Samuel Tetteh <khobbylynx55@gmail.com>
2023-05-02 07:30:06 +0000

    cors url, env and port

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const connect = async () => {
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors({origin: "http://localhost:5173", credentials: true}))
+app.use(cors({origin: "https://justclickjobs.netlify.app", credentials: true}))
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
@@ -44,7 +44,8 @@ app.use((err, req, res, next) => {
   return res.status(errorStatus).send(errorMessage);
 });
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
   connect();
   console.log(`Server running at http://localhost:5000`);
 });
